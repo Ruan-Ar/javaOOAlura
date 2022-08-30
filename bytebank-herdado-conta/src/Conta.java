8bdf96d5dece1ac39536090e93d5cf33099c5f2a
Ruan Andrade <ruantratos@gmail.com>
2022-08-30 16:12:15 -0300

    comentando saldo

--- bytebank-herdado-conta/src/Conta.java
+++ bytebank-herdado-conta/src/Conta.java
@@ -13,7 +13,7 @@ public abstract class Conta {
         //System.out.println("O total de contas é " + Conta.total);
         this.agencia = agencia;
         this.numero = numero;
-        this.saldo = 100;
+        //this.saldo = 100;
         //System.out.println("Estou criando uma conta " + this.numero);
     }
 
